606feabbfb15cd1e34e0bf86a3220eee067fe5b1
David Adelekan <38602193+adelekand@users.noreply.github.com>
2024-10-09 13:34:37 -0300

    Update environment.ts

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aa1b31614b9bb4ef1aedf1e1bfac5cf8-1334889971.ca-central-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
